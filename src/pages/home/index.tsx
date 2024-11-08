cfd832eeb6e28d9d6fa62b2df037035c5662881a
Athena Cheng <athenachengyc15@gmail.com>
2024-11-08 22:14:35 +0800

    feat : home page layout

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,3 +1,17 @@
+import { DifficultyBtn } from "../../components/difficulty-btn";
 export const Home = () => {
-	return <>Home</>;
+	return (
+		<div className="flex flex-col items-center justify-center p-8">
+			<p className="text-font-dark text-[92px]">SODOKU</p>
+			<div className="flex flex-col items-center justify-center space-y-4 mb-12">
+				<DifficultyBtn selected label="EASY" />
+				<DifficultyBtn selected label="MEDIUM" />
+				<DifficultyBtn selected label="HARD" />
+				<DifficultyBtn selected label="EXPERT" />
+			</div>
+			<button className="text-white border-theme bg-theme hover:bg-theme-hover hover:border-theme-hover transition border-4 border-solid rounded-xl w-80 h-20">
+				Go
+			</button>
+		</div>
+	);
 };
